2b33eccc573694001c6f5729a9b01c50cfca550c
mpenna <mpenna@NTI003>
2019-09-06 17:48:32 -0300

    header component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import Header from './Components/Header/Header'
 
 class App extends React.Component {
   constructor() {
@@ -8,20 +9,16 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <header class="tc ph4">
-          <h1 class="f3 f2-m f1-l fw2 black-90 mv3">
-            Speech Squares
-          </h1>
-        </header>
-        <nav class="pv4 ph3 ph5-m ph6-l">
-          <a class="link dim gray    f6 f5-ns dib mr3" href="#" title="Home">Home</a>
-          <a class="link dim gray    f6 f5-ns dib mr3" href="#" title="About">About</a>
-          <a class="link dim gray    f6 f5-ns dib mr3" href="#" title="Store">Store</a>
-          <a class="link dim gray    f6 f5-ns dib" href="#" title="Contact">Contact</a>
-        </nav>
-        <section class="w-80 center bg-light-gray pv4 ph3 ph5-m ph6-l">
-         <div>
-           Form and Squares here
+        <Header />
+        <section class="w-80 center bg-light-gray pt1 pb4 ph3 ph5-m ph6-l">
+          <nav class="pv4">
+            <a class="link dim gray    f6 f5-ns dib mr3" href="#" title="Home">Home</a>
+            <a class="link dim gray    f6 f5-ns dib mr3" href="#" title="About">About</a>
+            <a class="link dim gray    f6 f5-ns dib mr3" href="#" title="Store">Store</a>
+            <a class="link dim gray    f6 f5-ns dib" href="#" title="Contact">Contact</a>
+          </nav>
+          <div>
+            Form and Squares here
          </div>
         </section>
         <footer class="fixed bottom-0 w-100 pv4 ph3 ph5-m ph6-l mid-gray">
