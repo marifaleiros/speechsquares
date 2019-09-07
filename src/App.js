22501d8aee5f9da0133386035859d2321dcff16c
Mariana Faleiros <marifaleiros@hotmail.com>
2019-09-07 08:07:32 -0300

    start of form and button samples

--- src/App.js
+++ src/App.js
@@ -9,10 +9,22 @@ class App extends React.Component {
     return (
       <main className="app"> 
         <Header />
-        <section class="h-100 w-80 center bg-light-gray pt1 pb4 ph3 ph5-m ph6-l">
-          <div>
-            Form and Squares here
-         </div>
+        <section class="h-100 w-80 center bg-light-gray pb4">
+          <form class="bg-blue2 center pa4">
+            <fieldset class="cf bn ma0 pa0">
+              <div class="cf">
+                <input class="f6 f5-l input-reset bn fl black-80 bg-white pa3 lh-solid w-100 w-75-m w-80-l br2-ns br--left-ns" placeholder="type a 'sound' and add it to the board" type="text" name="email-address" value="" id="email-address"/>
+                <input class="f6 f5-l button-reset fl pv3 tc bn bg-animate bg-black-70 hover-bg-black white pointer w-100 w-25-m w-20-l br2-ns br--right-ns" type="submit" value="Add"/>
+              </div>
+            </fieldset>
+          </form>
+          <div className="pa2">
+            <div class="ba br2 b--light-silver shadow-1 fl w4 h4 bg-pink dim">
+              <p class="db link  tc pa4">
+                ão
+              </p>
+            </div>
+          </div>
         </section>
         <Footer />
       </main>
--- src/Components/Menu/Menu.jsx
+++ src/Components/Menu/Menu.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 const Menu = () => {
   return (
     <nav class="pv4">
-      <a class="link dim gray    f6 f5-ns dib mr3" href="#" title="Home">Home</a>
-      <a class="link dim gray    f6 f5-ns dib mr3" href="#" title="About">About</a>
-      <a class="link dim gray    f6 f5-ns dib mr3" href="#" title="Store">Store</a>
-      <a class="link dim gray    f6 f5-ns dib" href="#" title="Contact">Contact</a>
+      <a class="link dim near-white f6 f5-ns dib mr3" href="#" title="Home">Home</a>
+      <a class="link dim near-white f6 f5-ns dib mr3" href="#" title="About">About</a>
+      <a class="link dim near-white f6 f5-ns dib mr3" href="#" title="Store">Store</a>
+      <a class="link dim near-white f6 f5-ns dib" href="#" title="Contact">Contact</a>
     </nav>);
 };
 
