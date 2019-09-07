a7a87cc286a21f0693a8a81d984b62af009dbccc
Mariana Faleiros <marifaleiros@hotmail.com>
2019-09-07 09:02:29 -0300

    add button form

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Header from './Components/Header/Header'
 import Footer from './Components/Footer/Footer'
+import AddButtonForm from './Components/AddButtonForm/AddButtonForm'
 
 
 class App extends React.Component {
@@ -10,16 +11,9 @@ class App extends React.Component {
       <main className="app"> 
         <Header />
         <section class="h-100 w-80 center bg-light-gray pb4">
-          <form class="bg-blue2 center pa4">
-            <fieldset class="cf bn ma0 pa0">
-              <div class="cf">
-                <input class="f6 f5-l input-reset bn fl black-80 bg-white pa3 lh-solid w-100 w-75-m w-80-l br2-ns br--left-ns" placeholder="type a 'sound' and add it to the board" type="text" name="email-address" value="" id="email-address"/>
-                <input class="f6 f5-l button-reset fl pv3 tc bn bg-animate bg-black-70 hover-bg-black white pointer w-100 w-25-m w-20-l br2-ns br--right-ns" type="submit" value="Add"/>
-              </div>
-            </fieldset>
-          </form>
+          <AddButtonForm/>
           <div className="pa2">
-            <div class="ba br2 b--light-silver shadow-1 fl w4 h4 bg-pink dim">
+            <div class="ba br2 b--light-silver shadow-1 fl w4 h4 bg-pink dim pointer">
               <p class="db link  tc pa4">
                 ão
               </p>
