658bbf4bd67a76104ccd8d543d4d704fc0163e6c
mpenna <mpenna@NTI003>
2019-09-12 09:11:05 -0300

    listening to ENTER key press to add a square

--- src/Components/AddSquareForm/AddSquareForm.jsx
+++ src/Components/AddSquareForm/AddSquareForm.jsx
@@ -14,7 +14,9 @@ class AddSquareForm extends React.Component {
                 <input className="f6 f5-l input-reset bn fl black-80 bg-white pa3 lh-solid w-100 w-75-m w-80-l br2-ns br--left-ns"
                     placeholder="type a 'sound' and add it to the board"
                     type="text"
-                    onChange={this.updateState} />
+                    value={this.state.text}
+                    onChange={this.updateState}
+                    onKeyPress={this.checkEnter} />
                 <input className="f6 f5-l button-reset fl pv3 tc bn bg-animate bg-black-70 hover-bg-black white pointer w-100 w-25-m w-20-l br2-ns br--right-ns"
                     type="submit"
                     value="Add"
@@ -26,10 +28,19 @@ class AddSquareForm extends React.Component {
         this.setState({ text: e.target.value })
     }
 
+    checkEnter = (e) => {
+        if (e.key === 'Enter') {
+            this.onAdd();
+        }
+    }
+
     onAdd = () => {
-        if(this.state.text){
-        if (this.props.onAddSquare)
-            this.props.onAddSquare(this.state.text);
+        if (this.state.text) {
+            if (this.props.onAddSquare)
+            {
+                this.props.onAddSquare(this.state.text);
+                this.setState({ text: '' });
+            }
         }
     }
 };
