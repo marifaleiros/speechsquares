fac7b4220e3c4261c5a562b9ec2ca28e4d1e2995
mpenna <mpenna@NTI003>
2019-09-12 09:04:33 -0300

    showing something when there are no squares

--- src/Components/SquareList/SquareList.jsx
+++ src/Components/SquareList/SquareList.jsx
@@ -1,17 +1,31 @@
 import React from 'react';
 import Square from '../Square/Square'
 import './SquareList.css'
+import '@fortawesome/fontawesome-free/css/all.min.css'
 
 class SquareList extends React.Component {
     render() {
         const { squares } = this.props;
         return (
             <div className="center mw8 list flex flex-wrap bg-near-white">
-                {squares.map(s => {
-                    return <Square key={s.id} square={s} deleteSquare={this.props.deleteSquare} />
-                })}
+                {squares.length === 0 ? this.renderEmpty() : this.renderSquares(squares)}
             </div>
         );
+    }
+
+    renderEmpty() {
+        return (
+            <div className="center tc cf pa4">
+                <i class="fa fa-5x fa-cube" aria-hidden="true"></i>
+                <p>Add squares to get started :)</p>
+            </div>
+        )
+    }
+
+    renderSquares(squares){
+        return squares.map(s => {
+            return <Square key={s.id} square={s} deleteSquare={this.props.deleteSquare} />
+        })
     }
 };
 
